8f9a83ec7276d4cabc7bb3e437b59fc229c88cd0
Nida Ilyas <38118425+ni-da@users.noreply.github.com>
2018-09-28 12:39:53 +0200

    Assignment1
    not ALL DONE, need to check the EXIT- command

---
+++ Assignment1_InformationServer/info_client.py
@@ -0,0 +1,15 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 2000))
+
+s.sendall("TIME")
+msg = s.recv(60)
+print msg
+
+s.sendall("IP")
+msg = s.recv(60)
+print msg
+
+s.sendall("EXIT")
+s.close()
--- Assignment1_InformationServer/info_script.py
+++ Assignment1_InformationServer/info_script.py
@@ -1,9 +1,34 @@
 import socket
+import time
+import sys
+
+
+def close_server(conn, s):
+    conn.close
+    s.close
+    sys.exit()
+
 
 host = 'localhost'
 port = 2000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
-s.listen(1)
-conn, addr = s.accept()
-print('Connected by %s' % addr)
+
+s.listen(3)
+while 1:
+    conn, addr = s.accept()  # returns a NEW socket
+    cliAdr, cliPort = addr
+    print "Connected with ", cliAdr
+    while True:
+        data = conn.recv(1024)
+        if data == "IP":
+            conn.sendall(cliAdr)
+        if data == "TIME":
+            conn.sendall(time.ctime())
+        if data == "EXIT":
+            close_server(conn, s)
+            break
+        if not data:
+            break
+    conn.close()
+s.close()
